4a94d6a9cf64e454164d9eee28b1b9fbbe88ebf5
snomiao <snomiao@gmail.com>
2024-06-21 19:03:03 +0000

    feat(snoflow): init
    init

--- index.ts
+++ index.ts
@@ -5,9 +5,90 @@ import {
   toArray,
   toPromise,
   toString,
+  type ReadableLike,
 } from "web-streams-extensions";
+type TransformStreamReturnType<TS extends TransformStream> =
+  TS extends TransformStream<any, infer R> ? R : never;
+type TSRT<TS extends TransformStream> = TransformStreamReturnType<TS>;
+type SFRT<FN extends (...args: any[]) => TransformStream> =
+  TransformStreamReturnType<ReturnType<FN>>;
+type ChangeReturnType<T> = T extends (...args: any[]) => any
+  ? (...args: Parameters<T>) => SFRT<T>
+  : never;
+
+export type snoflow<T> = ReadableStream<T> & {
+  buffer: (...args: Parameters<typeof buffers<T>>) => snoflow<T[]>;
+  debounce: (...args: Parameters<typeof debounces<T>>) => snoflow<T>;
+  done: () => Promise<void>;
+  filter(): snoflow<NonNullable<T>>;
+  filter(fn: (x: T) => Awaitable<any>): snoflow<T>;
+  flatMap: <R>(...args: Parameters<typeof flatMaps<T, R>>) => snoflow<R>;
+  join(fn: (s: WritableStream<T>) => void | any): snoflow<T>;
+  join(stream: ReadableStream<T>): snoflow<T>;
+  limit: (...args: Parameters<typeof limits<T>>) => snoflow<T>;
+  map: <R>(...args: Parameters<typeof maps<T, R>>) => snoflow<R>;
+  peek: (...args: Parameters<typeof peeks<T>>) => snoflow<T>;
+  reduce: <S>(...args: Parameters<typeof reduces<T, S>>) => snoflow<S>;
+  skip: (...args: Parameters<typeof skips<T>>) => snoflow<T>;
+  tail: (...args: Parameters<typeof tails<T>>) => snoflow<T>;
+  tee(fn: (s: ReadableStream<T>) => void | any): snoflow<T>;
+  tee(stream: WritableStream<T>): snoflow<T>;
+  throttle: (...args: Parameters<typeof throttles<T>>) => snoflow<T>;
+} & (T extends any[]
+    ? { flat: (...args: Parameters<typeof flats<T>>) => snoflow<T[number]> }
+    : {});
+
+// @ts-ignore
+export const snoflow: <T>(
+  src:
+    | Promise<T>
+    | Iterable<T>
+    | AsyncIterable<T>
+    | (() => Iterable<T> | AsyncIterable<T>)
+    | ReadableLike<T>
+) => snoflow<T> = (src) => {
+  const r = from(src);
+  return (
+    // @ts-ignore
+    /* @ts-ignore */ Object.assign(r, {
+      buffer: (...args) => snoflow(r.pipeThrough(buffers(...args))), // @ts-ignore
+      debounce: (...args) => snoflow(r.pipeThrough(debounces(...args))), // @ts-ignore
+      done: () => r.pipeTo(nils()), // @ts-ignore
+      filter: (...args) => snoflow(r.pipeThrough(filters(...args))), // @ts-ignore
+      flatMap: (...args) => snoflow(r.pipeThrough(flatMaps(...args))), // @ts-ignore
+      flat: (...args) => snoflow(r.pipeThrough(flats(...args))), // @ts-ignore
+      join: (...args) => snoflow(r.pipeThrough(joins(...args))), // @ts-ignore
+      limit: (...args) => snoflow(r.pipeThrough(limits(...args))), // @ts-ignore
+      map: (...args) => snoflow(r.pipeThrough(maps(...args))), // @ts-ignore
+      peek: (...args) => snoflow(r.pipeThrough(peeks(...args))), // @ts-ignore
+      reduce: (...args) => snoflow(r.pipeThrough(reduces(...args))), // @ts-ignore
+      skip: (...args) => snoflow(r.pipeThrough(skips(...args))), // @ts-ignore
+      tail: (...args) => snoflow(r.pipeThrough(tails(...args))), // @ts-ignore
+      tee: (...args) => snoflow(r.pipeThrough(tees(...args))), // @ts-ignore
+      throttle: (...args) => snoflow(r.pipeThrough(throttles(...args))), // @ts-ignore
+    })
+  );
+};
+
+snoflow([1, 2, 3])
+  .buffer(2)
+  .debounce(100)
+  .filter()
+  .map((n) => [String(n)])
+  .flat()
+  .flatMap((n) => [String(n)])
+  .tee((s) => s.pipeTo(nils()))
+  .limit(1)
+  .map(() => 1)
+  .peek(() => {})
+  .reduce(0, (a, b) => a + b)
+  .skip(1)
+  .tail(1)
+  .throttle(100)
+  .done();
 
 type Awaitable<T> = Promise<T> | T;
+
 export function maps<T, R>(fn: (x: T, i: number) => Awaitable<R>) {
   let i = 0;
   return new TransformStream<T, R>({
@@ -87,10 +168,10 @@ export function throttles<T>(t: number, keepLast = true) {
     },
   });
 }
-
-export function filters<T>(): TransformStream<T, NonNullable<T>>;
-export function filters<T>(fn: (x: T) => Awaitable<any>): TransformStream<T, T>;
-export function filters(fn?: (x: any) => Awaitable<any>) {
+export const filters: {
+  <T>(): TransformStream<T, NonNullable<T>>;
+  <T>(fn: (x: T) => Awaitable<any>): TransformStream<T, T>;
+} = (fn?: (arg: any) => any) => {
   return new TransformStream({
     transform: async (chunk, ctrl) => {
       if (fn) {
@@ -102,9 +183,10 @@ export function filters(fn?: (x: any) => Awaitable<any>) {
       }
     },
   });
-}
+};
+from([1, 2, 3]).pipeThrough(filters());
 
-export function flatmaps<T, R>(fn: (x: T) => Promise<R[]> | R[]) {
+export function flatMaps<T, R>(fn: (x: T) => Promise<R[]> | R[]) {
   return new TransformStream<T, R>({
     transform: async (chunk, ctrl) => {
       (await fn(chunk)).map((e) => ctrl.enqueue(e));
@@ -129,7 +211,7 @@ export function reduces<T, S>(
 }
 export function tails<T>(n = 1) {
   let chunks: T[] = [];
-  return new TransformStream<T>({
+  return new TransformStream<T, T>({
     transform: async (chunk, ctrl) => {
       chunks.push(chunk);
       if (chunks.length > n) chunks.shift();
@@ -142,7 +224,7 @@ export function tails<T>(n = 1) {
 export const firsts = limits;
 export const heads = limits;
 export function limits<T>(n = 1) {
-  return new TransformStream<T>({
+  return new TransformStream<T, T>({
     transform: async (chunk, ctrl) => {
       ctrl.enqueue(chunk);
       if (!n--) ctrl.terminate();
@@ -150,7 +232,7 @@ export function limits<T>(n = 1) {
   });
 }
 export function skips<T>(n = 1) {
-  return new TransformStream<T>({
+  return new TransformStream<T, T>({
     transform: async (chunk, ctrl) => {
       if (n <= 0) ctrl.enqueue(chunk);
       else n--;
@@ -161,7 +243,7 @@ export function skips<T>(n = 1) {
 export function buffers<T>(n: number) {
   let chunks: T[] = [];
   if (n <= 0) throw new Error("Buffer size must be greater than 0");
-  return new TransformStream<T>({
+  return new TransformStream<T, T[]>({
     transform: async (chunk, ctrl) => {
       chunks.push(chunk);
       if (chunks.length === n) {
