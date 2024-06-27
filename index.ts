2abf981a8c71077e597b170912b2d7cea78760bd
snomiao <snomiao@gmail.com>
2024-06-27 13:54:08 +0000

    fix(main): wip writable
    wip writable

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import DIE from "@snomiao/die";
 import { sortBy, type Ord } from "rambda";
 import {
   from as wseFrom,
@@ -214,10 +213,10 @@ export const snoflow = <T>(src: flowSource<T>): snoflow<T> => {
     get readable() {
       return r;
     },
-    get writable() {
-      DIE(new Error("WIP"));
-      return new WritableStream();
-    },
+    // get writable() {
+    //   DIE(new Error("WIP"));
+    //   return new WritableStream();
+    // },
     through: (...args: Parameters<typeof _throughs>) =>
       snoflow(r.pipeThrough(_throughs(...args))),
     mapAddField: (
