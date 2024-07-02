a6711efd4d7914c2669c32ac20fdd39b36dd8de5
snomiao <snomiao@gmail.com>
2024-07-02 16:28:17 +0900

    fix(main): re order peek fn
    re order peek fn

--- peeks.ts
+++ peeks.ts
@@ -5,8 +5,8 @@ export function peeks<T>(fn: (x: T, i: number) => Awaitable<void | any>) {
   let i = 0;
   return new TransformStream<T, T>({
     transform: async (chunk, ctrl) => {
-      await fn(chunk, i++);
       ctrl.enqueue(chunk);
+      await fn(chunk, i++);
     },
   });
 }
