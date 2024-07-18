15bc7002dddca67a02b999065c09498d63415bf1
snomiao <snomiao@gmail.com>
2024-07-18 19:23:29 +0000

    fix(main): logs with console.log
    logs with console.log

--- logs.ts
+++ logs.ts
@@ -1,3 +1,4 @@
+import type { Awaitable } from "./Awaitable";
 import { peeks } from "./peeks";
 import { throughs } from "./throughs";
 
@@ -5,6 +6,8 @@ import { throughs } from "./throughs";
  * log the value and index and return as original stream, handy to debug.
  * Note: stream won't await the log function.
  */
-export function logs<T>(logFn: (x: T, i: number) => void = console.log) {
-  return throughs(peeks<T>(logFn));
+export function logs<T>(
+  mapFn: (x: T, i: number) => Awaitable<any> = (s, i) => s
+) {
+  return throughs(peeks<T>(async (e, i) => console.log(await mapFn(e, i))));
 }
