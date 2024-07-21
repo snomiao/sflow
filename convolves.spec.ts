9c8210b598ce1c7da032126694efd8942799657b
snomiao <snomiao@gmail.com>
2024-07-21 05:51:22 +0000

    refactor(main): format
    format

--- convolves.spec.ts
+++ convolves.spec.ts
@@ -3,7 +3,7 @@ import sflow from ".";
 import { convolves } from "./convolves";
 
 it("convolve 1 in pipeline", async () => {
-  expect(await toArray(await sflow([1, 2, 3, 4]).convolve(1))).toEqual([
+  expect(await sflow([1, 2, 3, 4]).convolve(1).toArray()).toEqual([
     [1],
     [2],
     [3],
@@ -11,29 +11,26 @@ it("convolve 1 in pipeline", async () => {
   ]);
 });
 it("convolve 1", async () => {
-  expect(
-    await toArray(await sflow([1, 2, 3, 4]).pipeThrough(convolves(1)))
-  ).toEqual([[1], [2], [3], [4]]);
+  expect(await sflow([1, 2, 3, 4]).through(convolves(1)).toArray()).toEqual([
+    [1],
+    [2],
+    [3],
+    [4],
+  ]);
 });
 it("convolve 2", async () => {
-  expect(
-    await toArray(await sflow([1, 2, 3, 4]).pipeThrough(convolves(2)))
-  ).toEqual([
+  expect(await sflow([1, 2, 3, 4]).through(convolves(2)).toArray()).toEqual([
     [1, 2],
     [2, 3],
     [3, 4],
   ]);
 });
 it("convolve 3", async () => {
-  expect(
-    await toArray(await sflow([1, 2, 3, 4]).pipeThrough(convolves(3)))
-  ).toEqual([
+  expect(await sflow([1, 2, 3, 4]).through(convolves(3)).toArray()).toEqual([
     [1, 2, 3],
     [2, 3, 4],
   ]);
 });
 it("convolve smaller array", async () => {
-  expect(
-    await toArray(await sflow([1, 2, 3, 4]).pipeThrough(convolves(5)))
-  ).toEqual([]);
+  expect(await sflow([1, 2, 3, 4]).through(convolves(5)).toArray()).toEqual([]);
 });
