982c22f3754383a626111ee91c6c7563edc9b8db
snomiao <snomiao@gmail.com>
2024-07-23 20:24:51 +0000

    fix(uniq): use filters as kernel
    use filters as kernel

--- uniqs.ts
+++ uniqs.ts
@@ -1,37 +1,34 @@
 import type { Awaitable } from "./Awaitable";
-import { maps } from "./maps";
-import { reduces } from "./reduces";
+import { filters } from "./filters";
 import { throughs } from "./throughs";
 
 /** uniq by a new Set(), note: use === to compare */
-export const uniqs = <T>(): TransformStream<T, T> =>
-  throughs((s) =>
-    s
-      .pipeThrough(
-        reduces({ set: new Set<T>(), next: null as T }, (s, x: T) => {
-          if (s.set.has(x)) return;
-          s.set.add(x);
-          s.next = x;
-          return s;
-        })
-      )
-      .pipeThrough(maps((s) => s.next))
+export const uniqs = <T>(): TransformStream<T, T> => {
+  const set = new Set<T>();
+  return throughs((s) =>
+    s.pipeThrough(
+      filters((x: T) => {
+        if (set.has(x)) return false;
+        set.add(x);
+        return true;
+      })
+    )
   );
+};
 
-/** uniq by a new Map(), note: use === to compare keys, so don't return object from keyFn.*/
+/** uniq by a new Map(), Note: use === to compare keys */
 export const uniqBys = <T, K>(
   keyFn: (x: T) => Awaitable<K>
-): TransformStream<T, T> =>
-  throughs((s) =>
-    s
-      .pipeThrough(
-        reduces({ set: new Map<K, T>(), next: null as T }, async (s, x: T) => {
-          const key = await keyFn(x);
-          if (s.set.has(key)) return;
-          s.set.set(key, x);
-          s.next = x;
-          return s;
-        })
-      )
-      .pipeThrough(maps((s) => s.next))
+): TransformStream<T, T> => {
+  const set = new Set<K>();
+  return throughs((s) =>
+    s.pipeThrough(
+      filters(async (x: T) => {
+        const key = await keyFn(x);
+        if (set.has(key)) return false;
+        set.add(key);
+        return true;
+      })
+    )
   );
+};
