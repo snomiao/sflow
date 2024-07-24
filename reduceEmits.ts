aabd9085270e29f8449be7e3d029f78f5ea69e72
snomiao <snomiao@gmail.com>
2024-07-24 16:02:22 +0000

    fix(main): emitter
    emitter

--- reduceEmits.ts
+++ reduceEmits.ts
@@ -1,15 +1,15 @@
 import type { Awaitable } from "./Awaitable";
 export const reduceEmits: {
   <T, S, R>(
-    fn: (state: S, x: T, i: number) => Awaitable<{ state: S; emit: R }>,
+    fn: (state: S, x: T, i: number) => Awaitable<{ next: S; emit: R }>,
     state: S
   ): TransformStream<T, R>;
 } = (fn, _state) => {
   let i = 0;
   return new TransformStream({
     transform: async (chunk, ctrl) => {
-      const { state, emit } = await fn(_state, chunk, i++);
-      _state = state;
+      const { next, emit } = await fn(_state, chunk, i++);
+      _state = next;
       ctrl.enqueue(emit);
     },
   });
