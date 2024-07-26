44211dcec079f80d4af0e4d7773946f544483460
snomiao <snomiao@gmail.com>
2024-07-26 06:56:33 +0000

    fix(main): enqueue order
    enqueue order

--- pageStream.ts
+++ pageStream.ts
@@ -3,16 +3,16 @@ export type PageFetcher<Data, Cursor> = (
   cursor: Cursor
 ) => Awaitable<{ data: Data; next?: Cursor | null }>;
 export function pageStream<Data, Cursor>(
-  initialCursor: Cursor,
+  initialQuery: Cursor,
   fetcher: PageFetcher<Data, Cursor>
 ): ReadableStream<Data> {
-  let curr: Cursor = initialCursor;
+  let query: Cursor = initialQuery;
   return new ReadableStream({
     pull: async (ctrl) => {
-      const { data, next } = await fetcher(curr);
-      if (null == next) return ctrl.close();
-      curr = next;
+      const { data, next } = await fetcher(query);
       ctrl.enqueue(data);
+      if (null == next) return ctrl.close();
+      query = next;
     },
   });
 }
