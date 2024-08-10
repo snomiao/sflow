9bd39f7a49be931b1f627f151b528c950f1e151b
snomiao <snomiao@gmail.com>
2024-08-10 17:15:31 +0000

    🔧 refactor: Update logs.ts function parameters
    - Simplified the MapFnIndexed parameter in the logs.ts file to receive an optional index argument.
    - Removed the unused MapFnUnary type definition for code clarity and maintenance.
    - Refactored the logs function to handle the map function more efficiently and made the code easier to read and maintain.

--- logs.ts
+++ logs.ts
@@ -1,27 +1,20 @@
-import DIE from "phpdie";
 import type { Awaitable } from "./Awaitable";
 import { peeks } from "./peeks";
 import { bys } from "./throughs";
 
-type MapFnIndexed<T> = (x: T, i: number) => Awaitable<any>;
-type MapFnUnary<T> = (x: T) => Awaitable<any>;
+type MapFnIndexed<T> = (x: T, i?: number) => Awaitable<any>;
+// type MapFnUnary<T> = (x: T) => Awaitable<any>;
 
 /**
  * log the value and index and return as original stream, handy to debug.
  * Note: stream won't await the log function.
  */
-export function logs<T>(mapFn: MapFnIndexed<T> | MapFnUnary<T> = (s, i) => s) {
+export function logs<T>(mapFn: MapFnIndexed<T> = (s, i) => s) {
   return bys(
     peeks<T>(async (e, i) => {
-      const ret =
-        mapFn.length === 1
-          ? (mapFn as MapFnUnary<T>)(e)
-          : mapFn.length === 2
-          ? (mapFn as MapFnIndexed<T>)(e, 2)
-          : DIE("too much args");
+      const ret = mapFn(e, i);
       const val = ret instanceof Promise ? await ret : ret;
       console.log(typeof val === "string" ? val.replace(/\n$/, "") : val);
     })
   );
 }
-logs(JSON.stringify);
