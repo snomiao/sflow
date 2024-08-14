453f41c789682abb02873f4643d50944c9946de6
snomiao <snomiao@gmail.com>
2024-08-14 12:07:38 +0000

    🎨 refactor: Improve code structure for pageFlow and pageStream
    - Replaced direct Cursor type with Awaitable<Cursor> for initialCursor and initialQuery parameters in pageFlow and pageStream functions, respectively.
    - Modified query variable initialization in pageStream to use an object with a 'value' property instead of directly assigning initialQuery.
    - Updated query assignment in the pull function to access the 'value' property for better handling of the asynchronous nature of data fetching.
    - Removed unnecessary comment specifying the highWaterMark value in the ReadableStream creation for a cleaner code representation.

--- pageFlow.ts
+++ pageFlow.ts
@@ -1,8 +1,9 @@
+import type { Awaitable } from "./Awaitable";
 import { type PageFetcher, pageStream } from "./pageStream";
 import { sflow } from "./sflow";
 
 export function pageFlow<Data, Cursor>(
-  initialCursor: Cursor,
+  initialCursor: Awaitable<Cursor>,
   fetcher: PageFetcher<Data, Cursor>,
 ): sflow<Data> {
   return sflow(pageStream(initialCursor, fetcher));
