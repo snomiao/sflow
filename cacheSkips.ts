343b87a8bcfbb0989c00a1fa7ce24c8df815869c
snomiao <snomiao@gmail.com>
2024-08-21 19:12:56 +0000

    🎨 style: Improve code formatting in cacheSkips.ts
    - Enhance readability of the `cacheSkips` function by reformatting the `_options` parameter.
    - Use a more consistent style for object destructuring and multi-line object definitions.
    - Ensure proper alignment and spacing for better visual structure and consistency.
    - Add missing conditional check for cache equality before terminating the controller.

--- cacheSkips.ts
+++ cacheSkips.ts
@@ -10,34 +10,35 @@ import { never } from "./never";
  *
  * Once flow done, cache content, and skip cached content next time
  */
-
 export function cacheSkips<T>(
   store: {
     has?: (key: string) => Awaitable<boolean>;
     get: (key: string) => Awaitable<T[] | undefined>;
     set: (key: string, chunks: T[]) => Awaitable<any>;
   },
-  _options?: string |
-  {
-    /**
-     * Key could step name,
-     * or defaults to `new Error().stack` if you r lazy enough
-     */
-    key?: string;
-    /** defaults to 1 */
-    windowSize?: number;
-  }
+  _options?:
+    | string
+    | {
+        /**
+         * Key could step name,
+         * or defaults to `new Error().stack` if you r lazy enough
+         */
+        key?: string;
+        /** defaults to 1 */
+        windowSize?: number;
+      }
 ) {
   // parse options
   const {
-    key = new Error().stack ?? DIE("missing cache key"), windowSize = 1,
+    key = new Error().stack ?? DIE("missing cache key"),
+    windowSize = 1,
   } = typeof _options === "string" ? { key: _options } : _options ?? {};
   const chunks: T[] = [];
   const cachePromise = store.get(key);
   return new TransformStream({
     transform: async (chunk, ctrl) => {
       const cache = await cachePromise;
-      if (cache?.length) {
+      if (cache?.length && cache[0] === chunk) {
         await store.set(key, chunks.concat(...cache).slice(0, windowSize));
         ctrl.terminate();
         return await never();
