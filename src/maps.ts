121e407862afeab75ce5bfac8ca36fca419d27d1
snomiao <snomiao@gmail.com>
2025-06-13 08:22:08 +0000

    test(maps): add unit tests for maps function to ensure correct behavior and type safety feat(maps): update maps function to include type annotations for better type safety and clarity

--- src/maps.ts
+++ src/maps.ts
@@ -1,6 +1,8 @@
 import type { AsyncOrSync } from "ts-essentials";
 
-export function maps<T, R>(fn: (x: T, i: number) => AsyncOrSync<R>) {
+export function maps<T, R>(
+  fn: (x: T, i: number) => AsyncOrSync<R>
+): TransformStream<T, R> {
   let i = 0;
   return new TransformStream<T, R>({
     transform: async (chunk, ctrl) => {
