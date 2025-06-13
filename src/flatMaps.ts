b71257d6f36cbe3496b3340a80cf5e23cae7bef8
snomiao <snomiao@gmail.com>
2025-06-13 08:41:46 +0000

    feat(flatMaps.ts): update flatMaps function signature to specify return type as TransformStream<T, R> for better type safety and clarity

--- src/flatMaps.ts
+++ src/flatMaps.ts
@@ -2,7 +2,9 @@ import type { Awaitable } from "./Awaitable";
 
 // from([1, 2, 3]).pipeThrough(filters());
 
-export function flatMaps<T, R>(fn: (x: T, i: number) => Awaitable<R[]>) {
+export function flatMaps<T, R>(
+  fn: (x: T, i: number) => Awaitable<R[]>
+): TransformStream<T, R> {
   let i = 0;
   return new TransformStream<T, R>({
     transform: async (chunk, ctrl) => {
